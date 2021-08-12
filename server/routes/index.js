9fe8fbbfff0ccee1dd9b32125d6b3bd9bddb2dd7
Erin Grisham <erinmgrisham@gmail.com>
2021-08-12 11:34:55 -0400

    fixed routes, now able to retrieve db info via postman get request

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,12 +3,12 @@ const controllers = require('../controllers');
 
 //Required param: product_id
 //Optional params: page(int), count(int), sort(string)
-router.get('reviews/', controllers.getReviews);
+router.get('/', controllers.getReviews);
 
 //** */
-router.post('reviews/', controllers.postReview);
-router.get('reviews/meta', controllers.getMetaData);
-router.put('reviews/:review_id/helpful', controllers.markHelpful);
-router.put('reviews/:review_id/report', controllers.reportReview);
+router.post('/', controllers.postReview);
+router.get('/meta', controllers.getMetaData);
+router.put('/:review_id/helpful', controllers.markHelpful);
+router.put('/:review_id/report', controllers.reportReview);
 
 module.exports = router;
