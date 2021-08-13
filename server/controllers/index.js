b6a8a948cb2f5e89caed953b1a101fdf18679cec
Erin Grisham <erinmgrisham@gmail.com>
2021-08-13 11:48:10 -0400

    updated style of controllers for code consistency

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,64 +1,111 @@
 const models = require('../models');
 
-module.exports = {
+// {
+//   "product": "2",
+//   "page": 0,
+//   "count": 5,
+//   "results": [
+//     {
+//       "review_id": 5,
+//       "rating": 3,
+//       "summary": "I'm enjoying wearing these shades",
+//       "recommend": false,
+//       "response": null,
+//       "body": "Comfortable and practical.",
+//       "date": "2019-04-14T00:00:00.000Z",
+//       "reviewer_name": "shortandsweeet",
+//       "helpfulness": 5,
+//       "photos": [{
+//           "id": 1,
+//           "url": "urlplaceholder/review_5_photo_number_1.jpg"
+//         },
+//         {
+//           "id": 2,
+//           "url": "urlplaceholder/review_5_photo_number_2.jpg"
+//         },
+//         // ...
+//       ]
+//     },
+//     {
+//       "review_id": 3,
+//       "rating": 4,
+//       "summary": "I am liking these glasses",
+//       "recommend": false,
+//       "response": "Glad you're enjoying the product!",
+//       "body": "They are very dark. But that's good because I'm in very sunny spots",
+//       "date": "2019-06-23T00:00:00.000Z",
+//       "reviewer_name": "bigbrotherbenjamin",
+//       "helpfulness": 5,
+//       "photos": [],
+//     },
+//     // ...
+//   ]
+// }
 
-	getReviews: (req, res) => {
 
+const getReviews = (req, res) => {
 
-	  let productId = req.query.product_id;
+	let productId = req.query.product_id;
 
-			models.getReviews(productId, (err, data) => {
-				//console.log('DATA in controller: ', data);
+		models.getReviews(productId, (err, data) => {
+			//console.log('DATA in controller: ', data);
+			//let date = new Date(parseInt(data[0].date));
+
+			if (data.length) {
+
 				//let date = new Date(parseInt(data[0].date));
-
-				if (data.length) {
-
-					//let date = new Date(parseInt(data[0].date));
-					res.json(data).status(200);
-				} else {
-					res.send('No product with that ID!');
-				}
-
-			})
-	},
-
-	postReview: (req, res) => {
-
-		let review = req.body;
-
-		models.postReviews(review, () => {
-
-			res.sendStatus(201);
-
+				res.json(data).status(200);
+			} else {
+				res.send('No product with that ID!');
+			}
 		})
-	},
-	getMetaData: (req, res) => {
+};
 
-		models.getMetaData((err, metaData) => {
+const	postReview = (req, res) => {
 
-			res.json(metaData).status(200);
+	let review = req.body;
 
-		})
-	},
+	models.postReviews(review, () => {
 
-  markHelpful: (req, res) => {
+		res.sendStatus(201);
 
-		let reviewId = req.query.review_id;
+	})
+};
 
-		models.markHelpful(reviewId, () => {
+const	getMetaData = (req, res) => {
 
-			res.sendStatus(204);
-		})
-	},
+	models.getMetaData((err, metaData) => {
 
-  reportReview: (req, res) => {
+		res.json(metaData).status(200);
 
-		let reviewId = req.query.review_id;
+	})
+};
 
-		models.reportReview(reviewId, () => {
+const markHelpful = (req, res) => {
 
-			res.sendStatus(204);
-		})
+	let reviewId = req.query.review_id;
 
-	}
-}
+	models.markHelpful(reviewId, () => {
+
+		res.sendStatus(204);
+	})
+};
+
+const reportReview = (req, res) => {
+
+	let reviewId = req.query.review_id;
+
+	models.reportReview(reviewId, () => {
+
+		res.sendStatus(204);
+	})
+};
+
+
+module.exports = {
+	getReviews,
+  postReview,
+  getMetaData,
+  markHelpful,
+  reportReview
+};
