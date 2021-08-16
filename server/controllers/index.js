0467baa50c5ca49df85517214b0414f7029d88da
Erin Grisham <erinmgrisham@gmail.com>
2021-08-16 18:26:17 -0400

    cleaned up reviews GET route, added code to prevent reported reviews from being sent in response

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,159 +1,91 @@
 const models = require('../models');
 
-// {
-//   "product": "2",
-//   "page": 0,
-//   "count": 5,
-//   "results": [
-//     {
-//       "review_id": 5,
-//       "rating": 3,
-//       "summary": "I'm enjoying wearing these shades",
-//       "recommend": false,
-//       "response": null,
-//       "body": "Comfortable and practical.",
-//       "date": "2019-04-14T00:00:00.000Z",
-//       "reviewer_name": "shortandsweeet",
-//       "helpfulness": 5,
-//       "photos": [{
-//           "id": 1,
-//           "url": "urlplaceholder/review_5_photo_number_1.jpg"
-//         },
-//         {
-//           "id": 2,
-//           "url": "urlplaceholder/review_5_photo_number_2.jpg"
-//         },
-//         // ...
-//       ]
-//     },
-//     {
-//       "review_id": 3,
-//       "rating": 4,
-//       "summary": "I am liking these glasses",
-//       "recommend": false,
-//       "response": "Glad you're enjoying the product!",
-//       "body": "They are very dark. But that's good because I'm in very sunny spots",
-//       "date": "2019-06-23T00:00:00.000Z",
-//       "reviewer_name": "bigbrotherbenjamin",
-//       "helpfulness": 5,
-//       "photos": [],
-//     },
-//     // ...
-//   ]
-// }
-
 //product id returns array of review objects where 'id' corresponds to 'review_id' of photos table
 
 const getReviews = (req, res) => {
+  const productId = req.query.product_id || 5;
+  const page = req.query.page || 1;
+  const queryCount = req.query.count || 5;
+  const querySort = req.query.sort || 'helpful';
 
-	const productId = req.query.product_id || 5;
-	//let reviewId = req.query.review_id || 5;
-	const page = req.query.page || 1;
-	const queryCount = req.query.count || 5;
-	const querySort = req.query.sort || 'helpful';
+  models.getReviews(productId, (err, reviewData) => {
+    let dataToSend = {
+      product: productId,
+      page: page,
+      count: queryCount,
+      results: [],
+    };
 
-	//count number of callbacks and increment until we hit count
+    //checks if getReviews was successful
+    if (reviewData.length) {
+      const reviewsArr = [];
 
-		models.getReviews(productId, async (err, reviewData) => {
+      reviewData.forEach(review => {
+        models.getPhotos(review.id, (err, photoData) => {
+          if (err) {
+            console.log('Error retrieving photos in controller: ', err);
+          } else {
+            reviewsArr.push({
+              review_id: review.id,
+              rating: review.rating,
+              summary: review.summary,
+              recommend: JSON.parse(review.recommend),
+              response: JSON.parse(review.response),
+              body: review.body,
+              date: new Date(parseInt(review.date)),
+              reviewer_name: review.reviewer_name,
+              helpfulness: review.helpfulness,
+              photos: photoData,
+            });
 
-			let dataToSend = {
-				product: productId,
-        page: page,
-        count: queryCount,
-        results: []
-			}
-
-			// Checks if getReviews was successful
-			if (reviewData.length) {
-
-				const len = reviewData.length;
-
-				const reviewsArr = [];
-
-				const getPhotosInfo = async (review) => {
-					const reviewId = review.id;
-
-						const photosResponse = models.getPhotos(reviewId, async (err, photoData) => {
-							if (err) {
-								console.log('Error retrieving photos in controller: ', err);
-							} else {
-
-								reviewsArr.push(
-									{
-										review_id: reviewId,
-										rating: review.rating,
-										summary: review.summary,
-										recommend: JSON.parse(review.recommend),
-										response: JSON.parse(review.response),
-										body: review.body,
-										date: new Date(parseInt(review.date)),
-										reviewer_name: review.reviewer_name,
-										helpfulness: review.helpfulness,
-										photos: photoData
-									});
-
-								if (reviewsArr.length === len) {
-									dataToSend.results = reviewsArr;
-									res.json(dataToSend).status(200);
-								}
-
-							}
-						})
-				}
-				reviewData.map(async (review) => {
-					return getPhotosInfo(review);
-				});
-
-			} else {
-				res.send('No product with that ID!');
-			}
-		})
+            //tells us when callback is finished and we can send data back
+            if (reviewsArr.length === reviewData.length) {
+              dataToSend.results = reviewsArr;
+              res.json(dataToSend).status(200);
+            }
+          }
+        });
+      });
+    } else {
+      res.send('No product with that ID!');
+    }
+  });
 };
 
-const	postReview = (req, res) => {
+const postReview = (req, res) => {
+  let review = req.body;
 
-	let review = req.body;
-
-	models.postReviews(review, () => {
-
-		res.sendStatus(201);
-
-	})
+  models.postReviews(review, () => {
+    res.sendStatus(201);
+  });
 };
 
-const	getMetaData = (req, res) => {
-
-	models.getMetaData((err, metaData) => {
-
-		res.json(metaData).status(200);
-
-	})
+const getMetaData = (req, res) => {
+  models.getMetaData((err, metaData) => {
+    res.json(metaData).status(200);
+  });
 };
 
 const markHelpful = (req, res) => {
+  let reviewId = req.params.review_id;
 
-	let reviewId = req.params.review_id;
-
-	models.markHelpful(reviewId, () => {
-
-		res.sendStatus(204);
-	})
+  models.markHelpful(reviewId, () => {
+    res.sendStatus(204);
+  });
 };
 
 const reportReview = (req, res) => {
+  let reviewId = req.params.review_id;
 
-	let reviewId = req.params.review_id;
-
-	models.reportReview(reviewId, () => {
-
-		res.sendStatus(204);
-	})
+  models.reportReview(reviewId, () => {
+    res.sendStatus(204);
+  });
 };
 
 module.exports = {
-	getReviews,
+  getReviews,
   postReview,
   getMetaData,
   markHelpful,
-  reportReview
+  reportReview,
 };
--- server/models/index.js
+++ server/models/index.js
@@ -1,55 +1,39 @@
 const db = require('../../database');
 
-//json_build_object()
-//json_agg()
-
-//you can use Date('string') in JS with the 13 char format and it returns it in a legible format. I'm doing the inverse to store new reviews Date.now().toString().
-
 const getReviews = (productId, callback) => {
-
-  //query combines reviews table with photos table join on review_id, id
-  //DO NOT RETURN if reported = true
-
-  let params = [];
-
-  //console.log('product id in model: ', productId);
 
   let queryString = `SELECT * FROM reviews WHERE product_id=${productId}`;
 
-  db.query(queryString, params, (err, results) => {
+  db.query(queryString, (err, results) => {
     if (err) {
       console.log('Error getting all reviews from database: ', err);
       callback(err);
     } else {
-      console.log('Successfully retrieved review data');
-      console.log(results);
-      callback(null, results);
+      //remove reported reviews
+      results.forEach(result => {
+        if (result.reported === 'false') {
+          const index = results.indexOf(result);
+          results = results.splice(index, 1, result);
+          callback(null, results);
+        }
+      })
     }
   });
 };
 
 //no path for this, just a helper function for /reviews endpoint
-//try to promisify this function
 const getPhotos = (reviewId, callback) => {
 
-  let params = [];
-
-  //console.log('review id in getPhotos model: ', reviewId);
-
-  //SELECT id, url
   let queryString = `SELECT id, url FROM photos WHERE review_id=${reviewId}`;
 
-  db.query(queryString, params, (err, results) => {
+  db.query(queryString, (err, results) => {
     if (err) {
       console.log('Error getting photos from database: ', err);
       callback(err);
     } else {
-      console.log('Successfully retrieved photo data');
-      console.log('Photos: ', results);
       callback(null, results);
     }
   });
-
 }
 
 const postReview = (reviewData, callback) => {
