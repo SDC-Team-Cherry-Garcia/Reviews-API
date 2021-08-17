1877a615e80b543f8d0671a2c946851312363d6d
Erin Grisham <erinmgrisham@gmail.com>
2021-08-16 21:18:55 -0400

    trying loader.io

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const router = require('./server/routes');
 app.use(express.json());
 
 // Set up our routes
-app.use('', router);
+app.use('/reviews', router);
 
 app.get('/reviews', (req, res) => res.json({ message: 'Hello World' }));
 
